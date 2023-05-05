36a82325a933bdf96517651c9ab2716ce05081f7
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-05-05 00:11:17 -0300

    agrego config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    inProduction: true,
+    api: {},
+    bot: {
+        // Only in development
+        TELEGRAM_TOKEN: "",
+        DB_NAME: "database",
+        DB_USER: "root",
+        DB_PASSWORD: "",
+        DB_HOST: "localhost"
+    }
+}
